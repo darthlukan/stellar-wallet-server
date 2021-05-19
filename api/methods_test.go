7e16eb8568da84a08a3d812d303a77729bcdf581
Brian Tomlinson <btomlins@redhat.com>
2021-05-18 23:02:24 -0700

    Add healthz endpoint
    - Include tests for Health method
    - Fix test for CreateAccount to match actually testing the JSON data in
      some form
    - Add healthz to router and openshift/10-deployment.yaml

--- api/methods_test.go
+++ api/methods_test.go
@@ -1,8 +1,34 @@
 package api
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestHealth(t *testing.T) {
+	rw := httptest.NewRecorder()
+	c, engine := gin.CreateTestContext(rw)
+
+	engine.GET("/healthz", Health)
+	request, err := http.NewRequestWithContext(c, http.MethodGet, "/healthz", nil)
+	if err != nil {
+		t.Fatalf("request: err = %v; want nil", err)
+	}
+
+	recorder := httptest.NewRecorder()
+	engine.ServeHTTP(recorder, request)
+
+	jdata := make(map[string]interface{})
+
+	json.Unmarshal(recorder.Body.Bytes(), &jdata)
+
+	assert.EqualValues(t, jdata["status"], http.StatusOK, "the two status should match")
+}
 
 /*
 * Stub tests because we don't need to duplicate
--- stellar-wallet-server.go
+++ stellar-wallet-server.go
@@ -9,6 +9,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/ping", api.Ping)
+	router.GET("/healthz", api.Health)
 
 	router.GET("/account/:address", api.GetAccount)
 	router.POST("/account", api.CreateAccount)
