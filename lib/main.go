9e9514932c367b0cd01c53dbd3003a09d1dd6efc
Brian Tomlinson <btomlins@redhat.com>
2021-05-19 20:46:39 -0700

    Add GetHorizonClient function
    - We need this so that we make sure we're only hitting the test net
      when testing and the pubnet when in production
    - Other client types (or even overrides) may come into play, so we use a
      switch statement with a default to testnet, just in case.

--- lib/main.go
+++ lib/main.go
@@ -1,6 +1,21 @@
 package lib
 
-import "github.com/stellar/go/keypair"
+import (
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+)
+
+func GetHorizonClient(clientType string) horizonclient.Client {
+	switch clientType {
+	case "test":
+		client := horizonclient.DefaultTestNetClient
+	case "prod":
+		client := horizonclient.DefaultPublicNetClient
+	default:
+		client := horizonclient.DefaultTestNetClient
+	}
+	return client
+}
 
 func CreateKeyPair() (string, string, error) {
 	pair, err := keypair.Random()
