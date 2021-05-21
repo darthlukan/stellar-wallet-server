e4e3ebd142a490e4c4e91669ef555a92b7212b47
Brian Tomlinson <btomlins@redhat.com>
2021-05-20 20:49:30 -0700

    Wire up payment to the API package

--- api/methods.go
+++ api/methods.go
@@ -33,3 +33,39 @@ func GetAccount(c *gin.Context) {
 	account.GetAccount(address, c)
 	return
 }
+
+func SendPayment(c *gin.Context) {
+	var data map[string]interface{}
+	err := c.BindJSON(&data)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"data":   nil,
+			"error":  err,
+		})
+		return
+	}
+
+	srcSecKey := data["seed"].(string)
+	destAddr := data["dest"].(string)
+	amount := data["amount"].(string)
+	assetType := data["asset"].(string)
+
+	txn, err := SendPayment(environ, srcSecKey, destAddr, amount, assetType)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": http.StatusInternalServerError,
+			"data":   nil,
+			"error":  err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"data":   txn,
+		"error":  nil,
+	})
+	return
+
+}
